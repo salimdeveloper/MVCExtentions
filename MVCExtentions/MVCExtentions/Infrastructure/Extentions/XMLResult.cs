f94d4062dcc0bc852f0b402a460ec41ec7b1320c
salimdeveloper <salimalamdeveloper@gmail.com>
2018-10-31 12:19:20 +0530

    Added Custom Action Result XMLResult

--- MVCExtentions/MVCExtentions/Infrastructure/Extentions/XMLResult.cs
+++ MVCExtentions/MVCExtentions/Infrastructure/Extentions/XMLResult.cs
@@ -1,9 +1,23 @@
 ﻿using System.Web.Mvc;
+using System.Xml.Serialization;
 
 namespace MVCExtentions.Infrastructure.Extentions
 {
-    public class XMLResult
+    public class XMLResult : ActionResult
     {
+        private object _data;
 
+        public XMLResult(object data)
+        {
+            _data = data;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            XmlSerializer serializer = new XmlSerializer(_data.GetType());
+            var response = context.HttpContext.Response;
+            response.ContentType = "text/xml";
+            serializer.Serialize(response.Output, _data);
+        }
     }
 }
