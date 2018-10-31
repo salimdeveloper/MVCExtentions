967f4ee7cd553078c30f5c3bdcb785efd83a5995
salimdeveloper <salimalamdeveloper@gmail.com>
2018-10-31 22:51:14 +0530

    Exception Handling Filter to send ajax exception response.

--- MVCExtentions/MVCExtentions/Global.asax.cs
+++ MVCExtentions/MVCExtentions/Global.asax.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using MVCExtentions.App_Start;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -13,6 +10,7 @@ namespace MVCExtentions
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
         }
     }
 }
---
+++ MVCExtentions/MVCExtentions/Infrastructure/Extentions/ExceptionLoggingFilter.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace MVCExtentions.Infrastructure.Extentions
+{
+    public class ExceptionLoggingFilter : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            //send ajax response
+            if (filterContext.HttpContext.Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+            {
+                filterContext.Result = new JsonResult
+                {
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                    Data = new
+                    {
+                        Message = "An error has occured. Please try again later",
+                    }
+                };
+            }
+            filterContext.HttpContext.Response.StatusCode = 500;
+            filterContext.ExceptionHandled = true;
+
+        }
+
+    }
+}
