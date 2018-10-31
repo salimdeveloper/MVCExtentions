2219f69bc260c467b7f3d5a3955c20fb2c7a2697
salimdeveloper <salimalamdeveloper@gmail.com>
2018-10-31 13:40:15 +0530

    Implemented CSVResult Extention Inheriting FileResult

---
+++ MVCExtentions/MVCExtentions/Infrastructure/Extentions/CSVResult.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.IO;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCExtentions.Infrastructure.Extentions
+{
+    public class CSVResult : FileResult
+    {
+        private IEnumerable _data;
+
+        public CSVResult(IEnumerable data, string fileName) : base("text/csv")
+        {
+            _data = data;
+            FileDownloadName = fileName;
+        }
+        protected override void WriteFile(HttpResponseBase response)
+        {
+            var builder = new StringBuilder();
+            var stringWriter = new StringWriter(builder);
+
+            foreach (var item in _data)
+            {
+                var properties = item.GetType().GetProperties();
+                foreach (var prop in properties)
+                {
+                    stringWriter.Write(GetValue(item, prop.Name));
+                    stringWriter.Write(",");
+                }
+                stringWriter.WriteLine();
+            }
+            response.Write(builder);
+        }
+
+        private static string GetValue(object item, string propName)
+        {
+            return item.GetType().GetProperty(propName).GetValue(item, null).ToString() ?? "";
+        }
+    }
+}
