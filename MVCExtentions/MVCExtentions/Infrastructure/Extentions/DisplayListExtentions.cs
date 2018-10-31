72cd91146ccd501543ea533cddcd446c0dfce03e
salimdeveloper <salimalamdeveloper@gmail.com>
2018-10-31 17:00:37 +0530

    Added HTMLHelper Extention DisplayList

---
+++ MVCExtentions/MVCExtentions/Infrastructure/Extentions/DisplayListExtentions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MVCExtentions.Infrastructure.Extentions
+{
+    public static class DisplayListExtentions
+    {
+        public static MvcHtmlString DisplayList(this HtmlHelper helper, List<string> data)
+        {
+            var ulBuilder = new TagBuilder("ul");
+            foreach (var item in data)
+            {
+                var liBuilder = new TagBuilder("li");
+                liBuilder.SetInnerText(item);
+                liBuilder.InnerHtml += liBuilder.ToString();
+            }
+            return new MvcHtmlString(ulBuilder.ToString());
+        }
+    }
+}
