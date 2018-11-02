b2f2ceee7958d777c2dfd0cc08d1fb014a352b55
salimdeveloper <salimalamdeveloper@gmail.com>
2018-11-02 19:05:32 +0530

    Implemented Custom BirthDate Over 18+ Validator

---
+++ MVCExtentions/MVCExtentions/Infrastructure/Extentions/Validation/BirthdateValidator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCExtentions.Infrastructure.Extentions.Validation
+{
+    public class BirthdateValidator : ValidationAttribute
+    {
+        public BirthdateValidator()
+        {
+            ErrorMessage = "Please enter a valid birth date. You must be 18 or older to apply.";
+        }
+        public override bool IsValid(object value)
+        {
+            DateTime enterDate;
+            if (DateTime.TryParse(value.ToString(), out enterDate))
+            {
+                if (enterDate > DateTime.Now.AddYears(-18))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
